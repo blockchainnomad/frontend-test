f236da3fe2aa77e8e4126aa33b99e9d71c728d0c
dev-jacobs <40536266+blockchainnomad@users.noreply.github.com>
2023-06-08 22:34:43 +0700

    Update gugudan.js

--- gugudan.js
+++ gugudan.js
@@ -1,8 +1,23 @@
-for (let i = 2; i <= 9; i++) {
-  for (let j = 1; j <= 9; j++) {
-    if ((i * j) % 4 == 0) {
-      continue;
-    }
-    console.log(i + "*" + j + "=" + i * j);
+let result1 = "";
+
+for (let i = 1; i <= 9; i++) {
+  if (i === 4) {
+    continue;
   }
+  for (let j = 2; j <= 5; j++) {
+    result1 += j + "*" + i + "=" + j * i + "\t";
+  }
+  result1 += "\n";
 }
+
+let result2 = "";
+for (let k = 1; k <= 9; k++) {
+  if (k === 4) {
+    continue;
+  }
+  for (let l = 6; l <= 9; l++) {
+    result2 += l + "*" + k + "=" + l * k + "\t";
+  }
+  result2 += "\n";
+}
+console.log(result1 + "\n" + result2);
